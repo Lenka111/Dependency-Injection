bbc801b5aed39e8e4e1a84be41b66868a9fc5b62
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-06-14 15:26:49 -0500

    Add files via upload

---
+++ TrackCoach.java
@@ -0,0 +1,25 @@
+//Elena Voinu
+package com.springdemo;
+
+public class TrackCoach implements Coach {
+    private FortuneService fortuneService;
+
+    public TrackCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    // no argument constructor
+    public TrackCoach() {
+
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return "Just Do It "+fortuneService.getFortune();
+    }
+}
