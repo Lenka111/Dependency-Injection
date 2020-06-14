d4f5b95c1b59a128d4ccd60b17f415a35062f449
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-06-14 15:26:07 -0500

    Add files via upload

---
+++ HappyFortuneService.java
@@ -0,0 +1,9 @@
+//dependency class
+package com.springdemo;
+
+public class HappyFortuneService implements FortuneService {
+    @Override
+    public String getFortune() {
+        return "Today is your lucky day";
+    }
+}
